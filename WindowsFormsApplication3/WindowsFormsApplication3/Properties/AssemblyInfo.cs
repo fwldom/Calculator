20ef4a3a34274e73d58f665f8a89d24f694379eb
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-08 13:03:14 +0430

    F

--- WindowsFormsApplication3/WindowsFormsApplication3/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication3/WindowsFormsApplication3/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyDescription("By Fwldom")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication3")]
+[assembly: AssemblyCompany("Fwldom")]
+[assembly: AssemblyProduct("Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
